d228ee8343d758f02ac17d14f11f0b35f1178a43
Janik Hotz <Neptune@Neptune.local>
2017-01-20 18:29:44 +0100

    faq done

---
+++ models/FAQ.js
@@ -0,0 +1,15 @@
+const keystone = require('keystone')
+const Types    = keystone.Field.Types
+
+
+const FAQ = new keystone.List('FAQ')
+
+FAQ.add({
+  content : {
+    type : Types.Html, wysiwig : true, height: 150
+  },
+  createdOn : { type : Types.Date, default: Date.now }
+})
+
+
+FAQ.register()
--- routes/views/faq.js
+++ routes/views/faq.js
@@ -3,9 +3,27 @@ const keystone = require('keystone')
 exports = module.exports = (req, res) => {
   let view = new keystone.View(req, res)
 
+  let locals = res.locals
+  locals.data = {
+    faqs : []
+  }
+
+  locals.section = 'faq'
+
   view.on('init', next => {
-    console.log('TODO')
-    next()
+    const query = keystone.list('FAQ').paginate({
+      page: req.query.page || 1,
+      perPage: 10,
+      maxPages: 10
+    })
+    .sort({
+      'createdOn' : 1
+    })
+
+    query.exec(function(err, docs) {
+      locals.data.faqs = docs
+      next(err)
+    })
   })
 
   view.render('faq')
