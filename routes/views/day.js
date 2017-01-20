0d7c3d2624797ddb037d9e51c96d64b6b94461e7
Janik Hotz <Neptune@Neptune.local>
2017-01-20 20:39:07 +0100

    dskjnsdlfn

--- routes/views/day.js
+++ routes/views/day.js
@@ -18,24 +18,36 @@ exports = module.exports = function(req, res) {
     movies : []
   }
 
-
   view.on('init', function(next) {
-    keystone.list('Movie').model
-      .find()
-      .where({'screenTime.year' : 2017})
-      .where({'screenTime.day' : req.params['day']})
-      .sort({
-        'screenTime.year'     : 1,
-        'screenTime.day'      : 1,
-        'screenTime.position' : 1
-      })
-      .exec(function(err, docs) {
-        locals.data.movies = docs
-        console.log('executed query for day ' + req.params.day)
-        console.log('found ' + docs.length + " documents" )
-        next(err)
-      })
+    if (locals.data.day < 11) {
+      keystone.list('Movie').model
+        .find()
+        .where({'screenTime.year' : 2017})
+        .where({'screenTime.day' : req.params['day']})
+        .sort({
+          'screenTime.year'     : 1,
+          'screenTime.day'      : 1,
+          'screenTime.position' : 1
+        })
+        .exec(function(err, docs) {
+          locals.data.movies = docs
+          console.log('executed query for day ' + req.params.day)
+          console.log('found ' + docs.length + " documents" )
+          next(err)
+        })
+      }
+      else {
+        keystone.list('Content').model
+          .findOne()
+          .where({'title' : 'LastDay'})
+          .exec(function(err, doc) {
+            if (doc) locals.data.content = doc.content
+            else locals.data.content = "No Content named 'Last Day' found"
+            next(err)
+          })
+      }
   })
+
 
 
   view.render('day')
