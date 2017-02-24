bd62f7410a8668e7e44d5b745fe034b0c3a56d4e
Janik Hotz <Neptune@Neptune.local>
2017-02-24 11:45:56 +0100

    updated index page post festival

--- routes/views/index.js
+++ routes/views/index.js
@@ -9,6 +9,14 @@ exports = module.exports = function (req, res) {
 	// item in the header navigation.
 	locals.section = 'home';
 
+	view.on('init', function(next) {
+		keystone.list('Content').model.findOne({title: 'LastDay'})
+			.exec(function(err, doc) {
+				locals.data = doc
+				next(err)
+			})
+	})
+
 	// Render the view
 	view.render('index');
 };
