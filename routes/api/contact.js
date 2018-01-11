0f1cdb06d25234b70ff22d3c25b80262b14c61c6
Janik Hotz <janikhotz@gmail.com>
2018-01-11 17:41:26 +0100

    debug

--- routes/api/contact.js
+++ routes/api/contact.js
@@ -2,15 +2,16 @@ const keystone = require('keystone');
 const Enquiry = keystone.list('Enquiry');
 
 exports = module.exports = function (req, res) {
+	var newEnqury = new Enquiry.model();
+	var updater = newEnqury.getUpdateHandler(req);
+
 	console.log('api/contact, req.body = ', req.body);
-	var newEnqury = new Enquiry.model({
-		name: { first: req.body.name },
-		email: req.body.email,
-		enquiryType: req.body.enquiryType,
-		message: req.body.message,
-	});
-	console.log(newEnqury);
-	newEnqury.save(function (err) {
+
+	updater.process(req.body, {
+		flashErrors: false,
+		fields: 'name, email, enquiryType, message',
+		errorMessage: 'There was a problem submitting your enquiry:',
+	}, function (err) {
 		if (err) {
 			res.status(500).send(err);
 		} else {
